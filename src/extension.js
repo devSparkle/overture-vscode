ee94089984437c7adfab12f4d63e1105b39a1caf
Ricardo Loureiro <github@devsparkle.me>
2023-09-21 15:16:23 +0100

    Add file nesting

--- src/extension.js
+++ src/extension.js
@@ -7,10 +7,25 @@ const config = vscode.workspace.getConfiguration()
 function activate(context) {
 	config.update("robloxLsp.runtime.plugin", context.asAbsolutePath("src/roblox-lsp-plugin.lua"), true)
 	
+	config.update("explorer.fileNesting.enabled", true, true)
+	config.update("explorer.fileNesting.expand", false, true)
+	config.update("explorer.fileNesting.patterns", {
+		"*.lua": "${capture}.meta.*",
+		"*.luau": "${capture}.meta.*",
+		
+		"*.server.lua": "${capture}.meta.*",
+		"*.server.luau": "${capture}.meta.*",
+		"*.client.lua": "${capture}.meta.*",
+		"*.client.luau": "${capture}.meta.*"
+	}, true)
 }
 
 function deactivate() {
 	config.update("robloxLsp.runtime.plugin", undefined, true)
+	
+	config.update("explorer.fileNesting.enabled", undefined, true)
+	config.update("explorer.fileNesting.expand", undefined, true)
+	config.update("explorer.fileNesting.patterns", undefined, true)
 }
 
 module.exports = {
