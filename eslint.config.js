81ebdd792803758b02056de4706d898d19b2ccba
devSparkle <github@devsparkle.me>
2026-04-06 22:56:14 +0100

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
+
+
+export default defineConfig([
+	{ files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+	tseslint.configs.recommended,
+
+	{
+		rules: {
+			"no-extra-semi": "warn",
+			"semi": ["warn", "never"],
+			"indent": ["warn", "tab", { ignoreComments: true, SwitchCase: 1 }],
+			
+			// Prevents duplicate warnings
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": "warn",
+		}
+	}
+])
